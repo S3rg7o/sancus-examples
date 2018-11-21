e227d7dc06c6e9c12d0740578dd140cf6ef25436
S3rg7o <seminara.sergio7@gmail.com>
2018-11-21 10:56:10 +0100

    Run the attacks

--- attack-presaved-data/main.c
+++ attack-presaved-data/main.c
@@ -48,7 +48,7 @@ int main()
     pr_info("all done!");
     
     
-    /*// Starting memory accesses
+    // Starting memory accesses
     pr_info("trying to illegally access mem from main.c");
     for(i=0; i<N_DATA; i++) 
     {
@@ -56,7 +56,7 @@ int main()
     	printf("[main.c] Address 0x%.4x\n",mp);
   		stolen_data = *((uint16_t *)mp);
 		printf("[main.c] Data nr.%d at addr. 0x%.4x \t 0x%.4x \n",i, mp, stolen_data);
-    }*/
+    }
     
     pr_info("starting dma illegal access...");
     
